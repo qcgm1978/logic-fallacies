8dd30eddb4d6c5da828bd28179ee06b2f2d3cea7
zhanghl-k <zhanghl-k@glodon.com>
2018-01-22 18:27:59 +0800

    add

---
+++ es6/other.spec.js
@@ -0,0 +1,7 @@
+describe(' Array.from()', () => {
+    it(' Array.from() accepts a second argument that\'s a `map` function. ', () => {
+        let year=new Date().getFullYear(),arr=Array(5);
+        arr.should.instanceof(Array);
+        arr.length.should.equal(5+1)
+    })
+})
--- es6/test.spec.js
+++ es6/test.spec.js
@@ -1,55 +1,55 @@
 const chai = require('chai'), should = chai.should()
 describe('destructuring for easier data access', () => {
     it('object destructuring', () => {
-        let obj = {a: 1, b: 2};
-        let {a, b} = obj;
+        let obj = { a: 1, b: 2 };
+        let { a, b } = obj;
         (a).should.equal(1, '');
         b.should.equal(2);
     });
-    it('default values',()=>{
-        let obj = {a: 1, b: 2};
-        let {a, b,c} = obj;
-        (c===undefined).should.ok
+    it('default values', () => {
+        let obj = { a: 1, b: 2 };
+        let { a, b, c } = obj;
+        (c === undefined).should.ok
     });
     it('nested object destructuring', () => {
-        let obj={
-            a:{
-                b:1,
-                c:[0,2]
+        let obj = {
+            a: {
+                b: 1,
+                c: [0, 2]
             }
         }
-        let {a:{b,c}}=obj;
+        let { a: { b, c } } = obj;
         b.should.equal(1, '');
         c[1].should.equal(2);
-        let {a:{b:d,c:e}}=obj;
+        let { a: { b: d, c: e } } = obj;
         d.should.equal(1);
         (typeof a).should.equal('undefined');
     });
     it('array destructuring', () => {
-        let arr=[1,2,3]
-        let [,a,b]=arr;
+        let arr = [1, 2, 3]
+        let [, a, b] = arr;
         a.should.equal(2, '');
         b.should.equal(3);
-        [a,b]=[b,a]
+        [a, b] = [b, a]
         a.should.equal(3)
         b.should.equal(2)
     })
-    it('rest items',()=>{
-        let film=['The Known','Star Wars','Avenger']
-        let [a,...b]=film;
+    it('rest items', () => {
+        let film = ['The Known', 'Star Wars', 'Avenger']
+        let [a, ...b] = film;
         a.should.equal('The Known')
-        b.should.eql(['Star Wars','Avenger']).but.not.equal(['Star Wars','Avenger'])
+        b.should.eql(['Star Wars', 'Avenger']).but.not.equal(['Star Wars', 'Avenger'])
     })
     it('destructuring parameters', () => {
-        let func=({a,b,c,d})=>[a,b,c,d]
-        func({a:1,b:2}).should.eql([1,2,undefined,undefined], '');
-        let func1=({a,b,c,d}={})=>[a,b,c,d]
-        func1().should.eql([undefined,undefined,undefined,undefined])
-        func1({a:'a',c:'c'}).should.eql(['a',undefined,'c',undefined])
+        let func = ({ a, b, c, d }) => [a, b, c, d]
+        func({ a: 1, b: 2 }).should.eql([1, 2, undefined, undefined], '');
+        let func1 = ({ a, b, c, d } = {}) => [a, b, c, d]
+        func1().should.eql([undefined, undefined, undefined, undefined])
+        func1({ a: 'a', c: 'c' }).should.eql(['a', undefined, 'c', undefined])
     })
     it('default values for destructured parameters', () => {
-        let func=({a,b='b',c,d='d'}={})=>[a,b,c,d]
-        func().should.eql([undefined,'b',undefined,'d'], '');
-        func({a:'a',d:'D'}).should.eql(['a','b',undefined,'D'])
+        let func = ({ a, b = 'b', c, d = 'd' } = {}) => [a, b, c, d]
+        func().should.eql([undefined, 'b', undefined, 'd'], '');
+        func({ a: 'a', d: 'D' }).should.eql(['a', 'b', undefined, 'D'])
     })
 });
