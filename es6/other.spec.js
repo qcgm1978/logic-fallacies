808df813e231ae1435c4c56db11b257ebddb7c25
zhanghl-k <zhanghl-k@glodon.com>
2018-01-31 17:30:01 +0800

    a

--- es6/other.spec.js
+++ es6/other.spec.js
@@ -1,7 +1,15 @@
+const chai = require('chai'), should = chai.should();
 describe(' Array.from()', () => {
     it(' Array.from() accepts a second argument that\'s a `map` function. ', () => {
-        let year=new Date().getFullYear(),arr=Array(5);
+        let year = new Date().getFullYear(), arr = Array(5);
         arr.should.instanceof(Array);
         arr.length.should.equal(5);
+    });
+    it(' Swap variables', () => {
+        var a = 'world', b = 'hello';
+        [a, b] = [b, a];
+        console.log(a)
+        a.should.equal('hello')
+        b.should.equal('world')
     })
 })
