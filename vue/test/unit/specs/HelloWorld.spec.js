eff9857c9c7bfb860f5328672077bf593e7bf11f
zhanghl-k <zhanghl-k@glodon.com>
2018-02-07 11:25:37 +0800

    vue test

--- vue/test/unit/specs/HelloWorld.spec.js
+++ vue/test/unit/specs/HelloWorld.spec.js
@@ -8,4 +8,10 @@ describe('HelloWorld.vue', () => {
     expect(vm.$el.querySelector('.hello h1').textContent)
       .to.equal('Welcome to Your Vue.js App')
   })
+  it('should show all the links', () => {
+    const Constructor = Vue.extend(HelloWorld)
+    const vm = new Constructor().$mount();
+    expect(vm.$el.querySelectorAll('a').length)
+      .to.equal(9)
+  })
 })
