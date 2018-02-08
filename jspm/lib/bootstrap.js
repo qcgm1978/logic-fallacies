1c06748e5d6da3ca1a02b7ffef274ab2cc7c4f9f
qcgm1978 <zhanghl@strongunion.com.cn>
2018-02-08 20:46:20 +0800

    jspm

---
+++ jspm/lib/bootstrap.js
@@ -0,0 +1,8 @@
+import _ from 'lodash-node/modern/lang/isEqual.js';
+import $ from 'jquery';
+import underscore from 'myname';
+
+export function bootstrap() {
+    // bootstrap code here
+    document.write('bootstrap module')
+}
