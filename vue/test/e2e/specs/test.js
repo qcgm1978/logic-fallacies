d869c2ee28086f9e3c5feb946d5c0785ea881e7a
qcgm1978 <zhanghl@strongunion.com.cn>
2018-02-09 20:56:55 +0800

    a

--- vue/test/e2e/specs/test.js
+++ vue/test/e2e/specs/test.js
@@ -7,21 +7,21 @@ module.exports = {
     // default: http://localhost:8080
     // see nightwatch.conf.js
     const devServer = browser.globals.devServerURL
-    browser.url(devServer)
-      .waitForElementVisible('#app', 5000)
-      .getCssProperty("h1", "display", function (result) {
-        this.assert.equal(typeof result, "object");
-        this.assert.equal(result.status, 0);
-        this.assert.equal(result.value, 'inline');
-      })
-    // browser
-    //   .url(devServer)
+    // browser.url(devServer)
     //   .waitForElementVisible('#app', 5000)
-    //   .assert.elementPresent('.hello')
-    //   .assert.containsText('h1', 'test')
-    //   .assert.elementCount('img', 0)
+    //   .getCssProperty("h1", "display", function (result) {
+    //     this.assert.equal(typeof result, "object");
+    //     this.assert.equal(result.status, 0);
+    //     this.assert.equal(result.value, 'inline');
+    //   })
+    browser
+      .url(devServer)
+      .waitForElementVisible('#app', 5000)
+      .assert.elementPresent('.hello')
+      .assert.containsText('h1', 'test')
+      .assert.elementCount('img', 0)
 
-    //   .end()
+      .end()
 
   }
 }
