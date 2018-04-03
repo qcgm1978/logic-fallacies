383aa460078739f78982bb6fb2e732b8b20a066a
zhanghl-k <zhanghl-k@glodon.com>
2018-04-03 11:10:32 +0800

    ts

---
+++ mocha/others/ts.spec.ts
@@ -0,0 +1,15 @@
+import greeter from '../ts/greeter.ts'
+import { greeter1 } from '../ts/greeter.ts'
+
+import { expect } from 'chai'
+describe(`http://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html`, () => {
+    it(`Building your first TypeScript file`, () => {
+        expect(greeter('Hawking')).to.equal('Hello, Hawking')
+    });
+    it(`Type annotations`, () => {
+        let user = [0, 1, 2];
+        // expect(() => greeter1(user)).to.throw();
+        expect(greeter1(user)).to.equal();
+
+    });
+})
---
+++ mocha/ts/greeter.ts
@@ -0,0 +1,9 @@
+export default function greeter(person) {
+    return "Hello, " + person;
+}
+
+let user = "Jane User";
+export function greeter1(person: string) {
+    return "Hello, " + person;
+}
+// document.body.innerHTML = greeter(user);
