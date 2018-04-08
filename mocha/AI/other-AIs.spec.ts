e7072ce602cb4af707a8f12185e4a25e82daf2a4
zhanghl-k <zhanghl-k@glodon.com>
2018-04-08 09:30:00 +0800

    a

--- mocha/AI/other-AIs.spec.ts
+++ mocha/AI/other-AIs.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from 'chai'
-const brain = require('brain.js')
+import { Network } from 'neataptic'
+const brain = require('brain.js');
 describe(`https://blog.bitsrc.io/11-javascript-machine-learning-libraries-to-use-in-your-app-c49772cca46c`, () => {
     describe(`https://github.com/BrainJS/brain.js`, () => {
         it(` approximate the XOR function using brain.js`, () => {
@@ -21,4 +22,30 @@ describe(`https://blog.bitsrc.io/11-javascript-machine-learning-libraries-to-use
             expect(this.network.layers).to.an('object');
         })
     })
+    describe(`https://github.com/wagenaartje/neataptic`, () => {
+        it(`this network learns the XOR gate (through neuro-evolution)`, (done) => {
+            var network = new Network(2, 1);
+
+            var trainingSet = [
+                { input: [0, 0], output: [0] },
+                { input: [0, 1], output: [1] },
+                { input: [1, 0], output: [1] },
+                { input: [1, 1], output: [0] }
+            ];
+
+            const generator = async () => await network.evolve(trainingSet, {
+                equal: true,
+                error: 0.03
+            });
+            // generator().then(data => {
+
+            //     expect(data).to.eql();
+            //     done()
+            // }).catch(err => {
+            //     debugger
+            //     done();
+            // })
+            done()
+        });
+    });
 })
